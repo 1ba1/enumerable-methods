4b6918486d68d643be2206f780f5e8d07c4e6c9f
1ba1 <vvvalenti01@yahoo.it>
2019-03-19 16:51:39 +0100

    defined methods inside enumerable module

---
+++ enumerable.rb
@@ -0,0 +1,61 @@
+module Enumerable
+  def my_each
+		for item in self
+			yield(item)
+		end
+		return self if block_given?
+	end
+
+  def my_each_with_index
+    count = 0
+		for item in self
+			yield(item, count)
+      count += 1
+		end
+		return self if block_given?
+	end
+
+  def my_select
+    array = []
+    for item in self
+      array.push(item) if yield(item) == true
+    end
+    return array if block_given?
+  end
+
+  def my_all?
+    count = 0
+    for item in self
+      count +=1 if yield(item) == true
+    end
+    if count == self.length
+      return true
+    else
+      return false
+    end
+  end
+
+  def my_any?
+    count = 0
+    for item in self
+      count +=1 if yield(item) == true
+    end
+    if count > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def my_none?
+    count = 0
+    for item in self
+      count +=1 if yield(item) == true
+    end
+    if count == 0
+      return true
+    else
+      return false
+    end
+  end
+end
